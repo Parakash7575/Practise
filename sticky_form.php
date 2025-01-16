6c8d902a82ee5c52849d458442724c6d7bfe1695
Prakash <prakashshiraskar2001@gmail.com>
2025-01-16 22:56:25 +0530

    File Uploading code in php

--- sticky_form.php
+++ sticky_form.php
@@ -3,10 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sticky form</title>
+    <title>Sticky Form</title>
 </head>
 <body>
-    
-    
+    <?php
+        $name = "";
+        $surname = "";
+
+        if (isset($_POST['submit'])) {
+            // Fetch and sanitize user inputs
+            $name = htmlspecialchars($_POST['name']);
+            $surname = htmlspecialchars($_POST['surname']);
+
+            echo "<p>Name: $name<br>Surname: $surname</p>";
+        }
+    ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <label for="name">Enter Name:</label>
+        <input type="text" name="name" value="<?php echo $name; ?>"><br>
+
+        <label for="surname">Enter Surname:</label>
+        <input type="text" name="surname" value="<?php echo $surname; ?>" required><br>
+
+        <input type="submit" value="Submit" name="submit">
+    </form>
 </body>
 </html>
